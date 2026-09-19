1b49a0015c6cee33ff4831652f61a8b908502ab3
klow1931 <klow1931@gmail.com>
2026-09-19 03:21:21 -0400

    Offline-cache decision performance modules

--- sw.js
+++ sw.js
@@ -37,6 +37,8 @@ const ASSETS = [
   './src/product/decision-engine.js',
   './src/product/decision-backtest.js',
   './src/product/decision-feedback.js',
+  './src/product/decision-performance.js',
+  './src/product/decision-performance-ui.js',
   './src/product/history-cleanup.js',
   './src/product/history-cleanup-ui.js',
   './src/product/training-review.js',
